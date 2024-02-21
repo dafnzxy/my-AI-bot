e24067c8762ebcffc4643fef330b98ae8823505a
dafnzxy <143616930+dafnzxy@users.noreply.github.com>
2024-02-21 20:22:28 +0700

    Add files via upload

---
+++ bot_logic.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+def gen_pass(pass_length):
+    character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+    password = " "
+
+    for i in range (pass_length):
+        password += random.choice(character)
+
+
+def coins(detik):
+    face = random.randint( 1, 2)
+    if face == 1:
+        time.sleep(detik)
+        return 'koin atas'
+    else:
+        time.sleep(detik)
+        return 'koin bawah'
